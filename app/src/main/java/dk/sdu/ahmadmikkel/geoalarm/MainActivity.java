f5bce26635e4ee23023c8f2fcd87b47429e5c89c
Ahmad Hamid <AhmadRHamid@gmail.com>
2020-04-25 18:38:06 +0200

    Read from Firestore

--- app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MainActivity.java
+++ app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MainActivity.java
@@ -1,12 +1,22 @@
 package dk.sdu.ahmadmikkel.geoalarm;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.multidex.MultiDex;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
 
     RecyclerView recyclerView;
 
@@ -25,14 +35,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        s1 = getResources().getStringArray(R.array.test_array);
-        s2 = getResources().getStringArray(R.array.test_array2);
+        s1 = new String[2];
+        s2 = new String[2];
 
-        recyclerView = findViewById(R.id.recyclerView);
+        db = FirebaseFirestore.getInstance();
 
-        RecyclerAdapter adapter = new RecyclerAdapter(this, s1, s2, images);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        db.collection("test").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    int i = 0;
+                    for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                        Log.d("FIRESTORE", documentSnapshot.getId() + ": " + documentSnapshot.get("time"));
+                        s1[i] = (String) documentSnapshot.get("time");
+                        s2[i] = (String) documentSnapshot.get("label");
+                        i++;
+                    }
+
+                    recyclerView = findViewById(R.id.recyclerView);
+
+                    RecyclerAdapter adapter = new RecyclerAdapter(getApplicationContext(), s1, s2, images);
+                    recyclerView.setAdapter(adapter);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                } else {
+                    Log.w("FIRESTORE", "Error getting documents from Firestore", task.getException());
+                }
+            }
+        });
+
+        //s1 = getResources().getStringArray(R.array.test_array);
+        //s2 = getResources().getStringArray(R.array.test_array2);
+
+//        recyclerView = findViewById(R.id.recyclerView);
+//
+//        RecyclerAdapter adapter = new RecyclerAdapter(this, s1, s2, images);
+//        recyclerView.setAdapter(adapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
--- app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/RecyclerAdapter.java
+++ app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/RecyclerAdapter.java
@@ -40,7 +40,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     @Override
     public int getItemCount() {
-        return images.length;
+        return data1.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
