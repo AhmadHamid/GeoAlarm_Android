1112a9a8f7927a70906aa13e5f541de44b1b159b
Ahmad Hamid <AhmadRHamid@gmail.com>
2020-04-25 17:23:35 +0200

    Write to Firestore

--- app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MainActivity.java
+++ app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.multidex.MultiDex;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        MultiDex.install(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
