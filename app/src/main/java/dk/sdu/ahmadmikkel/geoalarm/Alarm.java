a279c73fb9289b9fd74196b06ffd2a1338e90728
Ahmad Hamid <AhmadRHamid@gmail.com>
2020-04-25 18:51:40 +0200

    Alarm init

---
+++ app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/Alarm.java
@@ -0,0 +1,50 @@
+package dk.sdu.ahmadmikkel.geoalarm;
+
+public class Alarm {
+    private int hour;
+    private int minute;
+    private String label;
+    private String location;
+
+    public Alarm() {
+    }
+
+    public Alarm(int hour, int minute, String label, String location) {
+        this.hour = hour;
+        this.minute = minute;
+        this.label = label;
+        this.location = location;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
