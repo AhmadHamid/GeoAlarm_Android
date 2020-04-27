c52ebc01534179fbf2565e1c4d797cf7f2159129
Ahmad Hamid <AhmadRHamid@gmail.com>
2020-04-27 05:24:34 +0200

    Floating Action Button

--- app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/AddAlarmActivity.java
+++ app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/AddAlarmActivity.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.DialogFragment;
 import androidx.multidex.MultiDex;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -35,35 +36,10 @@ public class AddAlarmActivity extends AppCompatActivity {
     public void setTime(int hour, int minute) {
         TextView timeLabel = findViewById(R.id.addEditTime);
         timeLabel.setText(hour + ":" + minute);
-
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-
-        addToFirestore(user);
-
-
-        Map<String, Object> user1 = new HashMap<>();
-        user1.put("first", "Alan");
-        user1.put("middle", "Mathison");
-        user1.put("last", "Turing");
-        user1.put("born", 1912);
-
-        addToFirestore(user1);
     }
 
-    private void addToFirestore(Map<String, Object> user1) {
-        db.collection("users").add(user1).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-            @Override
-            public void onSuccess(DocumentReference documentReference) {
-                Log.d("FIRESTORE", "DocumentSnapshot added with ID: " + documentReference.getId());
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.w("FIRESTORE", "Error adding document", e);
-            }
-        });
+    public void addAlarm(View view) {
+        //TODO: Lav alarm via. Alarms.createAlarm.
+        Snackbar.make(view, "Snackbar", Snackbar.LENGTH_LONG).setAction("Alarm set", null).show();
     }
 }
