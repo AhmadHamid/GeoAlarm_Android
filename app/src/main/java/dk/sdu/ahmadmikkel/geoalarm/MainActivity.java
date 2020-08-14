fd326c57586b237f524a2f8fc20d7992387b434b
muggel <muggel@pop-os.localdomain>
2020-08-14 17:59:36 +0200

    Added permission check

--- app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MainActivity.java
+++ app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MainActivity.java
@@ -1,14 +1,17 @@
 package dk.sdu.ahmadmikkel.geoalarm;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.multidex.MultiDex;
@@ -26,12 +29,15 @@ public class MainActivity extends AppCompatActivity implements Observer {
     RecyclerView recyclerView;
     RecyclerAdapter adapter;
 
+    private static final int REQUEST_CODE = 101;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         MultiDex.install(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getPermission();
 
         alarms.addObserver(this);
 
@@ -42,6 +48,14 @@ public class MainActivity extends AppCompatActivity implements Observer {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+    }
+
+    public void getPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
+            return;
+        }
     }
 
     public void alarmSettings(View v) {
--- app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MapsFragment.java
+++ app/src/main/java/dk/sdu/ahmadmikkel/geoalarm/MapsFragment.java
@@ -1,39 +1,30 @@
 package dk.sdu.ahmadmikkel.geoalarm;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
-
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
-    };
 
     @Nullable
     @Override
@@ -49,11 +40,15 @@ public class MapsFragment extends Fragment {
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+            mapFragment.getMapAsync(MapsFragment.this);
         }
     }
 
 
-
-
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+            LatLng loc = new LatLng( 55.37, 10.4090);
+            googleMap.addMarker(new MarkerOptions().position(loc).title("Marker in Sydney"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+    }
 }
